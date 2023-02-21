bbc4d0a95e2c517a501b544e4f335eeb274c019d
Marco Linke <birneos@gmx.net>
2023-02-21 10:53:15 +0100

    add vuex store and use it

--- laravel-vue-commerce/backend/src/main.js
+++ laravel-vue-commerce/backend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './index.css';
 import App from './App.vue'
+import store from "./store/index.js";
 
-
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .mount('#app')
