5f1b179869f2358618fd664cd0aa12a203663eb0
Marco Linke <birneos@gmx.net>
2023-02-22 13:16:13 +0100

    add failing store

--- laravel-vue-commerce/backend/src/main.js
+++ laravel-vue-commerce/backend/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './index.css'
 import App from './App.vue'
 import router from '../src/router/index'
+import store from '../src/store/index'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
