c0013e366cfcc803504ff933c1c12d22501170e5
Marco Linke <birneos@gmx.net>
2023-02-22 18:49:31 +0100

    refrence value sidebarOpened and toggle this

--- laravel-vue-commerce/backend/src/components/AppLayout.vue
+++ laravel-vue-commerce/backend/src/components/AppLayout.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="min-h-full flex bg-gray-100">
         <!--        SIDEBAR -->
-       <sidebar></sidebar>
+       <sidebar :style="[!sidebarOpened ? 'display:none' : 'display:block']"  class=""></sidebar>
         <!--        END SIDEBAR -->
 
         <div class="flex-1">
-            <TopHeader></TopHeader>
+            <TopHeader @toggle-sidebar="toggleSidebar"></TopHeader>
             <main class="p-6">
                 <div class="p-4 rounded bg-white">
                     <router-view></router-view>
@@ -17,6 +17,13 @@
 </template>
 
 <script setup>
+import {ref} from "vue"
 import Sidebar from "./Sidebar.vue";
 import TopHeader from "./TopHeader.vue";
+
+const sidebarOpened = ref(true);
+function toggleSidebar(){
+    sidebarOpened.value = !sidebarOpened.value
+    console.log(sidebarOpened.value);
+}
 </script>
