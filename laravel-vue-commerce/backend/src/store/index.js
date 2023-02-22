e88010f4565e15fb2aae4ff216ffed97bc5fb930
Marco Linke <birneos@gmx.net>
2023-02-22 13:16:06 +0100

    add failing store

---
+++ laravel-vue-commerce/backend/src/store/index.js
@@ -0,0 +1,13 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+    state: {
+        test: 124444
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+
+});
+
+export default store;
