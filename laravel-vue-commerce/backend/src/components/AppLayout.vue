7e15f6d35ce752e234d674f8c2c25f11574b236a
Marco Linke <birneos@gmx.net>
2023-02-22 16:52:06 +0100

    update AppLayout, create Sidear and Header

--- laravel-vue-commerce/backend/src/components/AppLayout.vue
+++ laravel-vue-commerce/backend/src/components/AppLayout.vue
@@ -7,21 +7,23 @@
   <body class="h-full">
   ```
 -->
-    <router-view></router-view>
-    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div class="w-full max-w-md space-y-8">
-            <div class="w-full max-w-md space-y-8">
-                <div>
-                     <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">AppLayout</h2>
-                    <p class="mt-2 text-center text-sm text-gray-600">
-                        Or
-                        {{ ' ' }}
-                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
-                    </p>
-                </div>
-            </div>
+
+    <div class="min-h-full flex">
+        <!--        SIDEBAR -->
+       <sidebar></sidebar>
+        <!--        END SIDEBAR -->
+
+        <div class="flex-1">
+            <TopHeader></TopHeader>
+            <main class="p-6">
+                <router-view></router-view>
+            </main>
         </div>
 
     </div>
 </template>
 
+<script setup>
+import Sidebar from "./Sidebar.vue";
+import TopHeader from "./TopHeader.vue";
+</script>
---
+++ laravel-vue-commerce/backend/src/components/Sidebar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="w-[200px] bg-indigo-700 rounded-l-xl text-white py-4 px-5">
+        <router-link :to="{name: 'app.dashboard'}" class="flex items-center rounded transition-colors p-2 hover:bg-black/30 font-light">
+            <span class="mr-2 text-gray-300">
+            <HomeIcon class="h-5 w-5"></HomeIcon>
+            </span>
+            <span class="text-xs">Dashboard</span>
+        </router-link>
+
+        <router-link :to="{name: 'app.dashboard'}" class="flex items-center rounded transition-colors p-2 hover:bg-black/30 font-light">
+            <span class="mr-2 text-gray-300">
+            <ViewColumnsIcon class="h-5 w-5"></ViewColumnsIcon>
+            </span>
+            <span class="text-xs">Products</span>
+        </router-link>
+
+        <router-link :to="{name: 'app.dashboard'}" class="flex items-center rounded transition-colors p-2 hover:bg-black/30 font-light">
+            <span class="mr-2 text-gray-300">
+            <UsersIcon class="h-5 w-5"></UsersIcon>
+            </span>
+            <span class="text-xs">Users</span>
+        </router-link>
+
+        <router-link :to="{name: 'app.dashboard'}" class="flex items-center rounded transition-colors p-2 hover:bg-black/30 font-light">
+            <span class="mr-2 text-gray-300">
+            <ChartBarIcon class="h-5 w-5"></ChartBarIcon>
+            </span>
+            <span class="text-xs">Report</span>
+        </router-link>
+    </div>
+
+
+
+
+
+</template>
+text-gray-300
+<script setup>
+
+
+    const props = defineProps({
+        title: String
+    })
+    import { LockClosedIcon, HomeIcon,UsersIcon,ViewColumnsIcon,ChartBarIcon } from '@heroicons/vue/24/outline'
+</script>
+
+<style scoped>
+
+</style>
