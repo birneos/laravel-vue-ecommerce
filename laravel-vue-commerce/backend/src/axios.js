14f0e554a53604f13a549a4728bafcb8f9447ea0
Marco Linke <birneos@gmx.net>
2023-02-23 15:42:18 +0100

    install axios and create axiosClient

---
+++ laravel-vue-commerce/backend/src/axios.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import store from "../src/store/index";
+import router from "../src/router/index";
+
+const axiosClient = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+});
+
+//You can intercept requests or responses before they are handled by then or catch.
+axiosClient.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${store.state.user.token}`;
+    return config;
+});
+
+//You can intercept requests or responses before they are handled by then or catch.
+axiosClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response.status === 401) {
+            sessionStorage.removeItem("TOKEN");
+            router.push({ name: "login" });
+        }
+        throw error;
+    }
+);
+
+export default axiosClient;
