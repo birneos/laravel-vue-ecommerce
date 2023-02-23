22ddcc6921e1f06f4b2ba51ddd082fe0caa27b01
Marco Linke <birneos@gmx.net>
2023-02-23 13:59:52 +0100

    add AuthController for Login and Logout

---
+++ laravel-vue-commerce/app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+            'remember' => 'boolean'
+        ]);
+
+        $b_remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if(!Auth::attempt($credentials,$b_remember)){
+            return response([
+                'message' =>'Email or password is incorrect',
+            ],422);
+        }
+
+        /**
+         * @var User $user
+         */
+        $user = Auth::user();
+
+        if(!$user->is_admin){
+            Auth::logout();
+
+            return response([
+                'message' =>'You don\'t have permission to authenticate as admin',
+            ],403);
+        }
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+           'user'=> $user,
+            'token' => $token
+        ]);
+    }
+
+    public function logout(){
+
+        /**
+         * @var \App\Models\User $user
+         */
+        $user = Auth::user();
+
+        $user->currentAccessToken()->delete();
+
+        return response([
+            '',
+            204
+        ]);
+
+    }
+}
