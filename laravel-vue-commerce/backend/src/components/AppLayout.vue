0916c82e0c37f67cce00651aaa56414a19c33c48
Marco Linke <birneos@gmx.net>
2023-02-23 07:56:29 +0100

    rename TopHeader to Navbar

--- laravel-vue-commerce/backend/src/components/AppLayout.vue
+++ laravel-vue-commerce/backend/src/components/AppLayout.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="min-h-full flex bg-gray-100">
         <!--        SIDEBAR -->
-       <sidebar :style="[!sidebarOpened ? 'display:none' : 'display:block']"  class=""></sidebar>
+        <sidebar :style="[!sidebarOpened ? 'display:none' : 'display:block']" class=""></sidebar>
         <!--        END SIDEBAR -->
 
         <div class="flex-1">
-            <TopHeader @toggle-sidebar="toggleSidebar"></TopHeader>
+            <Navbar @toggle-sidebar="toggleSidebar"></Navbar>
             <main class="p-6">
                 <div class="p-4 rounded bg-white">
                     <router-view></router-view>
@@ -19,10 +19,11 @@
 <script setup>
 import {ref} from "vue"
 import Sidebar from "./Sidebar.vue";
-import TopHeader from "./TopHeader.vue";
+import Navbar from "./Navbar.vue";
 
 const sidebarOpened = ref(true);
-function toggleSidebar(){
+
+function toggleSidebar() {
     sidebarOpened.value = !sidebarOpened.value
     console.log(sidebarOpened.value);
 }
