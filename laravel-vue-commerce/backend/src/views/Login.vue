df498f56f20de35a8227666142b0be36b932dd82
Marco Linke <birneos@gmx.net>
2023-02-24 09:19:31 +0100

    v-if="loading" spinner only shows if loading=true

--- laravel-vue-commerce/backend/src/views/Login.vue
+++ laravel-vue-commerce/backend/src/views/Login.vue
@@ -57,7 +57,7 @@
                             'hover:bg-indigo-500':  loading
                     }"
                 >
-                    <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                    <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
                          fill="none" viewBox="0 0 24 24">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
                                 stroke-width="4"></circle>
