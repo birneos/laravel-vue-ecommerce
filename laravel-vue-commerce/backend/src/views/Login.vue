c9d0f8c2ca6eb5010f254931aa21b14a468c51a0
Marco Linke <birneos@gmx.net>
2023-02-24 11:23:35 +0100

    add closing svg errormessage

--- laravel-vue-commerce/backend/src/views/Login.vue
+++ laravel-vue-commerce/backend/src/views/Login.vue
@@ -16,8 +16,13 @@
 
     <GuestLayout title="Sign in to your Account">
         <form class="mt-8 space-y-6" method="POST" @submit.prevent="login">
-            <div v-if="errorMsg" class="flex justify-center items-center py-3 px-5 bg-red-500 text-white rounded">
+            <div v-if="errorMsg" class="flex justify-between items-center py-3 px-5 bg-red-500 text-white rounded">
                 {{errorMsg}}
+                <span @click="errorMsg=''" class="w-8 h-8 flex justify-center items-center rounded transition-colors cursor-pointer hover:bg-black/20">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
             </div>
             <input type="hidden" name="remember" value="true"/>
             <div class="-space-y-px rounded-md shadow-sm">
