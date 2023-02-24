2837c88d76ae5a74c439d58d8aea327895beea46
Marco Linke <birneos@gmx.net>
2023-02-24 15:43:19 +0100

    add HasSlug+SlugOptions to Product Model (dont forget to write "use HasSlug"

--- laravel-vue-commerce/app/Models/Product.php
+++ laravel-vue-commerce/app/Models/Product.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
     use HasFactory;
+    use HasSlug;
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
