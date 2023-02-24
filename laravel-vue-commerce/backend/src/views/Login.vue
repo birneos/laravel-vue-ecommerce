7f8854fcdd182c486992210506c926b9d57d271d
Marco Linke <birneos@gmx.net>
2023-02-24 08:05:58 +0100

    add login fn for dispatching + @submit.prevent modifier

--- laravel-vue-commerce/backend/src/views/Login.vue
+++ laravel-vue-commerce/backend/src/views/Login.vue
@@ -15,22 +15,22 @@
 <template>
 
             <GuestLayout title="Sign in to your Account">
-                <form class="mt-8 space-y-6" action="#" method="POST">
+                <form class="mt-8 space-y-6" method="POST" @submit.prevent="login">
                     <input type="hidden" name="remember" value="true" />
                     <div class="-space-y-px rounded-md shadow-sm">
                         <div>
                             <label for="email-address" class="sr-only">Email address</label>
-                            <input id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
+                            <input v-model="user.email" id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
                         </div>
                         <div>
                             <label for="password" class="sr-only">Password</label>
-                            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
+                            <input v-model="user.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
                         </div>
                     </div>
 
                     <div class="flex items-center justify-between">
                         <div class="flex items-center">
-                            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+                            <input v-model="user.remember" id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
                             <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
                         </div>
 
@@ -53,8 +53,39 @@
 </template>
 
 <script setup>
+import {ref} from 'vue'
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
 import requestPassword from "./RequestPassword.vue";
 import GuestLayout from "../components/GuestLayout.vue";
+import store from "../store/index.js";
+import router from "../router/index.js";
+
+//create variables will be watched by the DOM
+let loading = ref(false);
+let errorMsg = ref("");
+
+const user = {
+    email: '',
+    password: '',
+    remember: false
+}
+
+function login(){
+
+    loading.value = true;
+
+    //Actions are triggered with the store.dispatch method
+    store.dispatch('login', user)
+        .then(()=>{
+            loading.value = false;
+            router.push({name: 'app.dashboard'})
+        })
+        .catch(
+            (response)=>{
+                loading.value = false;
+                errorMsg.value = response.data.message;
+            })
+
+}
 </script>
 
