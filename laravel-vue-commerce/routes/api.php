1fd65a077d549cf23cd89b4280c7c53f491aa233
Marco Linke <birneos@gmx.net>
2023-02-24 19:28:20 +0100

    add Product routes for api

--- laravel-vue-commerce/routes/api.php
+++ laravel-vue-commerce/routes/api.php
@@ -26,8 +26,9 @@ Route::middleware(['auth:sanctum','admin'])
 //            });
 
         Route::get('/user',[\App\Http\Controllers\AuthController::class,'getUser']);
+        Route::post('/logout',[\App\Http\Controllers\AuthController::class,'logout']);
+        Route::apiResource('/product',\App\Http\Controllers\ProductController::class);
 
-            Route::post('/logout',[\App\Http\Controllers\AuthController::class,'logout']);
 });
 
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
